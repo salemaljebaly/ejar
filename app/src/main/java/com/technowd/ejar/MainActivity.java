ffd4709cff1deeb1ea42d2fc1eee6fc3cfd578f9
salemaljebaly <salemaljebaly@gmail.com>
2019-10-17 13:03:36 +0200

    fix bugs and clean up code

--- app/src/main/java/com/technowd/ejar/MainActivity.java
+++ app/src/main/java/com/technowd/ejar/MainActivity.java
@@ -2,13 +2,11 @@ package com.technowd.ejar;
 
 
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.net.Uri;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,16 +18,10 @@ import android.widget.ImageView;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 import com.technowd.ejar.general.Functions;
 import com.technowd.ejar.model.RentPosts;
 import com.technowd.ejar.model.RentRecyclerAdapter;
@@ -44,10 +36,8 @@ import com.technowd.ejar.ui.UserRentsActivity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Objects;
 
-import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "mainActivity";
@@ -55,29 +45,21 @@ public class MainActivity extends AppCompatActivity {
     private Functions functions = new Functions(MainActivity.this);
     private FirebaseAuth mAuth;
     private FirebaseFirestore firebaseFirestore;
-    // Recycler view
-    private RecyclerView rentRecyclerView;
     private List<RentPosts> rentPosts;
-    private DocumentSnapshot lastVisible;
     RentRecyclerAdapter rentRecyclerAdapter;
     FirebaseUser user;
     private ImageView wifi_off;
-    private Boolean isFirstQueryLoaded = true;
-    private String searchText = "إختر المدينة";
-    private Query firstQuery;
-    private String searchBy = "none";
-    private String user_name,user_phone;
-    private Uri user_image;
-    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Log.e("serverTimestamp", "" +FieldValue.serverTimestamp()  );
+
         main_tool_bar = findViewById(R.id.main_tool_bar);
         setSupportActionBar(main_tool_bar);
         rentPosts = new ArrayList<>();
-        rentRecyclerView = findViewById(R.id.rentRecyclerView);
+        // Recycler view
+        RecyclerView rentRecyclerView = findViewById(R.id.rentRecyclerView);
         wifi_off = findViewById(R.id.wifi_off);
         // init firebase auth
         mAuth = FirebaseAuth.getInstance();
@@ -90,19 +72,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(int position) {
                 Log.e(TAG, "onItemClick: " + position );
-            }
-        });
-        // Recycler view scrolled
-        rentRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                Boolean reachedButton = !recyclerView.canScrollVertically(1);
-                if(reachedButton){
-                    // String desc = lastVisible.getString("rent_desc");
-                     // loadMoreRents();
-                    // functions.custom_toast(desc);
-                }
             }
         });
         firebaseFirestore = FirebaseFirestore.getInstance();
@@ -122,11 +91,12 @@ public class MainActivity extends AppCompatActivity {
         // ---------------------------------------------------------------------- //
         //searchBy(searchBy);
         loadData();
-        Log.e("searchText", "onCreate: "+ searchText );
+        String searchText = "إختر المدينة";
+        Log.e("searchText", "onCreate: "+ searchText);
     }
 
     public void loadData(){
-        firstQuery = firebaseFirestore.collection("Rents").orderBy("time",Query.Direction.DESCENDING);
+        Query firstQuery = firebaseFirestore.collection("Rents").orderBy("time", Query.Direction.DESCENDING);
         if(functions.CheckInternet()){ // check internet
             wifi_off.setVisibility(View.INVISIBLE);
             // get data from DB depends on time desc
@@ -148,32 +118,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    // load 3 by 3 rents
-    public void loadMoreRents(){ //
-        Query nextQuery = null;
-
-            nextQuery = firebaseFirestore.collection("Rents").orderBy("time",Query.Direction.DESCENDING)
-                .startAfter(lastVisible).limit(3);
-        if(functions.CheckInternet()){
-            nextQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-
-                    if(!queryDocumentSnapshots.isEmpty()) { // check if data not empty
-                        lastVisible = queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size() - 1); // take all data in DB
-                        for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
-                            if (doc.getType() == DocumentChange.Type.ADDED) {
-                                RentPosts localrentPosts = doc.getDocument().toObject(RentPosts.class);
-                                rentPosts.add(localrentPosts);
-                                rentRecyclerAdapter.notifyDataSetChanged();
-                            }
-                        }
-                    }
-                }
-            });
-        }
-    }
     // on start method check if user login then write his name in toolbar
     @Override
     protected void onStart() {
@@ -230,12 +174,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    // to check if user login or not
-    private boolean checkUser(){
-        FirebaseUser user = mAuth.getCurrentUser();
-        return user != null;
-    }
     // Add new rent
     public void addNewRent(View view) {
         if(user != null){
@@ -243,16 +181,5 @@ public class MainActivity extends AppCompatActivity {
         } else {
             functions.goToActivityByParam(LoginActivity.class);
         }
-    }
-    // change language of android os
-    public void setLocal(String lang){
-        lang = "ar";
-        Locale locale = new Locale(lang);
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        Configuration config = getResources().getConfiguration();
-        config.setLocale(locale);
-        getResources().updateConfiguration(config,displayMetrics);
-        Intent refresh = new Intent(this, MainActivity.class);
-        startActivity(refresh);
     }
 }
